9618582a1d9228e4439a93105c75428c807449ec
Matthew-Barstead <mbarstead@deadreckoning.consulting>
2020-12-20 15:10:06 -0500

    adding simple loading utility

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,33 @@
 # File containing project-specific utilities
+library(magrittr)
+
+#' Loads .RData files in a given directory.
+#' 
+#' By default the funciton loads all .RData files in a given directory. The user can override this behavior by providing
+#' A single file or vector of file names. There is no need to include .RData in the file name when identifying a subset 
+#' of files (or a single file). Note that this function assumes that the only period in the fil
+#' 
+#' @importFrom tools file_ext
+#' @importFrom magrittr %>%
+load_RData_files <- function(dir_path, filename = NULL) {
+    
+    if(is.null(filename)) {
+        files <- list.files(dir_path)
+        file_extensions <- lapply(
+            files, 
+            FUN = tools::file_ext
+        ) %>% unlist()
+        
+        rdata_files <- files[which(tolower(file_extensions) == 'rdata')]
+        
+        for(f in rdata_files) {
+            load(paste0(dir_path, '/', f), envir = .GlobalEnv)
+        }
+    }
+    
+    else {
+        for(f in filename) {
+            load(paste0(dir_path, '/', f), envir = .GlobalEnv)
+        }
+    }
+}
--- project_config.R
+++ project_config.R
@@ -1,1 +1,10 @@
 # File containing basic configurations and settings for project
+
+LOCAL_REPO <- '~/github/ATNL/grad_mh'
+POSTERIOR_OUTPUTS <- '/media/dr-owner/HDD1/ATNL_grad_mh_Posteriors'
+
+# Create a directory at the location above if one does not already exist
+if(!dir.exists(POSTERIOR_OUTPUTS)) {
+    dir.create(POSTERIOR_OUTPUTS)
+}
+
